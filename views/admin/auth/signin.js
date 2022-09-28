07a9a21add7d8a7df95f30423221b590540781f6
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-28 22:57:09 +0500

    refact: add base html for admin

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,7 +1,12 @@
-module.exports = () => `
-  <form method="POST">
-    <input name="email" type="email" placeholder="Enter your email"/>
-    <input name="password" type="password" placeholder="Enter your password"/>
-    <button>Sign In</button>
-  </form>
-`;
+const layout = require("../layout");
+
+module.exports = () =>
+  layout({
+    content: `
+      <form method="POST">
+        <input name="email" type="email" placeholder="Enter your email"/>
+        <input name="password" type="password" placeholder="Enter your password"/>
+        <button>Sign In</button>
+      </form>
+    `,
+  });
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,11 +1,16 @@
-module.exports = ({ req }) => `
-  <div>
-    Your id is: ${req.session.userId}
-    <form method="POST">
-      <input name="email" type="email" placeholder="Enter your email"/>
-      <input name="password" type="password" placeholder="Enter your password"/>
-      <input name="passwordConfirmation" type="password" placeholder="Enter your password again"/>
-      <button>Sign Up</button>
-    </form>
-  </div>
-`;
+const layout = require("../layout");
+
+module.exports = ({ req }) =>
+  layout({
+    content: `
+      <div>
+        Your id is: ${req.session.userId}
+        <form method="POST">
+          <input name="email" type="email" placeholder="Enter your email"/>
+          <input name="password" type="password" placeholder="Enter your password"/>
+          <input name="passwordConfirmation" type="password" placeholder="Enter your password again"/>
+          <button>Sign Up</button>
+        </form>
+      </div>
+    `,
+  });
