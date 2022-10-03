d8e06254a65360b6f9e64b26a40fafc3b30a344b
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-03 22:02:29 +0500

    remove trim() validator method from passwords

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -19,13 +19,11 @@ module.exports = {
 
   // validation logic for password
   requirePassword: check("password")
-    .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage("Must be between 4 and 20 characters"),
 
   // validation logic for password confirmation
   requirePasswordConfirmation: check("passwordConfirmation")
-    .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage("Must be between 4 and 20 characters")
     .custom((passwordConfirmation, { req }) => {
