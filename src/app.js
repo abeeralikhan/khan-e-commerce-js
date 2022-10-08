65c4dfd6c29be398aa447c37d486643b07dca426
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-09 04:18:30 +0500

    feat: add products router

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 
 const authRouter = require("./routes/admin/auth/auth.route");
-const productsRouter = require("./routes/admin/products/products.route");
+const adminProductsRouter = require("./routes/admin/products/products.route");
+const productsRouter = require("./routes/products/products.route");
 
 const app = express();
 
@@ -18,10 +19,13 @@ app.use(
 );
 
 // redirect the root endpoint to the products list endpoint
-app.get("/", (req, res) => res.redirect("/admin/products/list"));
+app.get("/", (req, res) => res.redirect("/products"));
 
 // admin routers
 app.use("/admin", authRouter);
-app.use("/admin/products", productsRouter);
+app.use("/admin/products", adminProductsRouter);
+
+// products router
+app.use("/products", productsRouter);
 
 module.exports = app;
